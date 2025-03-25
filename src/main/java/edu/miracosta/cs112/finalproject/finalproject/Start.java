76dc2a8cb1acb087494974bcfbc36c2ac96e2452
MCCLAB560WD05 <jzoch7@gmail.com>
2025-03-25 12:12:32 -0700

    Starting class

---
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/Start.java
@@ -0,0 +1,22 @@
+package edu.miracosta.cs112.finalproject.finalproject;
+
+public class Start {
+    String question;
+    String answer;
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+    public String getAnswer() {
+        return answer;
+    }
+}
+
